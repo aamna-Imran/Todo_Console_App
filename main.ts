8aa15ec5e56aad8e23b2f12a4438bbf34ecdea99
Aamna Imran <153712062+aamna-Imran@users.noreply.github.com>
2024-01-09 23:01:56 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,73 @@
+import inquirer from "inquirer";
+
+// make a string to store you todo
+
+let todos: string[] = ["Aamna", "ali"];
+
+async function createTodo(arr: string[]) {
+  do {
+    let todoOperations = await inquirer.prompt({
+      type: "list",
+      name: "select",
+      choices: ["add", "update", "view", "delete"],
+      message: "select an operation :",
+    });
+
+    // now define operation of each operator
+
+
+    // Add Todo
+    if (todoOperations.select == "add") {
+      let addTodo = await inquirer.prompt({
+        type: "input",
+        name: "todo",
+        message: "Add new Todo => ",
+      });
+      todos.push(addTodo.todo);
+      todos.forEach((element) => {
+        console.log(element);
+      });
+    }
+    // Update Todo
+    if (todoOperations.select == "update") {
+      let updateTodo = await inquirer.prompt({
+        type: "list",
+        name: "Todo",
+        message: "Select the Todo to be updated :",
+        choices: todos.map((val) => val),
+      });
+      let addTodo = await inquirer.prompt({
+        type: "input",
+        name: "todo",
+        message: "Add new Todo => ",
+      });
+      let newTodo = todos.filter((val) => val !== updateTodo.Todo);
+      todos = [...newTodo, addTodo.todo];
+      todos.forEach((element) => {
+        console.log(element);
+      });
+    }
+    // view all Todos
+    if (todoOperations.select == "view") {
+      todos.forEach((element) => {
+        console.log(element);
+      });
+    }
+    // Delete any Todo
+    if (todoOperations.select == "delete") {
+      let deleteTodo = await inquirer.prompt({
+        type: "list",
+        name: "Todo",
+        message: "Select the Todo to be deleted :",
+        choices: todos.map((val) => val),
+      });
+      let newTodo = todos.filter((val) => val !== deleteTodo.Todo);
+      todos = [...newTodo];
+      todos.forEach((element) => {
+        console.log(element);
+      });
+    }
+  } while (true);
+}
+
+createTodo(todos);
